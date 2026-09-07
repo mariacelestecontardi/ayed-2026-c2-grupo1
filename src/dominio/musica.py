236e423da6f389f8dc434f462bafbe685e4b7e3a
celesteContardi <mariacelestecontardid@gmail.com>
2026-09-06 21:11:41 -0300

    catalogo 5 primeras canciones

---
+++ src/dominio/musica.py
@@ -0,0 +1,40 @@
+CATALOGO = [
+    { "id": 1, 
+      "titulo": "Propuesta indecente", 
+      "artista": "Romeo Santos", 
+      "album": "Formula", 
+      "fecha_de_lanzamiento": 2014, 
+      "duracion": "03:55"
+    },
+
+    { "id": 2, 
+      "titulo": "M.A.I", 
+      "artista": "Milo j",
+      "album": "111",
+      "fecha_de_lanzamiento": 2023,
+      "duracion": "03:04"
+    },
+
+    { "id": 3,
+      "titulo": "Houdini",
+      "artista": "Dua Lipa",
+      "album": "Radical Optimism",
+      "fecha_de_lanzamiento": 2024,
+      "duracion": "03:05"
+    },
+    { "id": 4,
+      "titulo": "Right Now",
+      "artista": "Korn",
+      "album": "Take a look in the mirror",
+      "fecha_de_lanzamiento": 2003,
+      "duracion": "03:09"
+    },
+    { "id": 5,
+      "titulo": "The Emptiness Machine",
+      "artista": "Linkin Park",
+      "album": "From Zero",
+      "fecha_de_lanzamiento": 2024,
+      "duracion": "03:10"
+    }
+]
+
